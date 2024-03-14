3cb0484b83e069002bdc726166dfc8481cd7d962
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2024-03-14 10:14:05 -0500

    cambios

--- parcial1/src/componentes/HomeComponent.js
+++ parcial1/src/componentes/HomeComponent.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 //import './email.css';
 
@@ -7,22 +7,53 @@ function HomeComponent() {
     const [usuarios, setUsuarios] = useState([])
 
     useEffect(() => {
-        fetch('https://my.api.mockaroo.com/users.json?key=53df03a0')
+        fetch('https://raw.githubusercontent.com/JeffersonH0501/Parcial1/main/datos.json')
         .then(response => response.json())
-        .then(data => setUsuarios([...usuarios, ...data]))
+        .then(data => setUsuarios(data))
         .catch(error => console.error('Error fetching characters:', error));
     }, []);
 
+    const randomNumberInRange = (min, max) => {
+        return Math.floor(Math.random()
+            * (max - min + 1)) + min;
+    };
 
-    const [userSelect, setUserSelect] = useState(null); // Inicialmente null
+    const usuario = usuarios[0];
 
     return (
         <div className="home">
+            {usuario && (
+                <div className='contenidoUno'>
 
-            <div className='contenidoUno'>
-                <img src={carro.image} alt={carro.carModel} />
-            </div>
+                    <img src={usuario.imagen1} alt={usuario.nombre} />
 
+                    <div className="infoUsuario">
+                        <h1>{usuario.nombre}</h1>
+                        <p><strong>Nombre: </strong> {usuario.usuario}</p>
+                        <p><strong>Descripcion: </strong> {usuario.descripcion}</p>
+                        <p><strong>Posts: </strong> {usuario.posts}</p>
+                        <p><strong>Seguidores: </strong> {usuario.seguidores}</p>
+                        <p><strong>Seguidos: </strong> {usuario.seguidos}</p>
+
+                    </div>
+
+                    <h1>Imagenes</h1>
+                    <div className='contenidoImagenes'>
+
+                        {usuario.
+
+                        }
+                            {carros.map(carro => (
+                            <div className="carro" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={() => goDetail(carro)}>
+                                <img src={carro.image} alt={carro.carModel} />
+                                <p><strong>{carro.carModel}</strong> </p>
+                            </div>
+                        ))}
+
+
+                    </div>
+                </div>
+            )}
         </div>
     );
 
